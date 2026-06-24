947116b824de635fe3d2577a3f96d0f3d4e126dc
flebt_yY <hygor.danilo.santos@escola.pr.gov.br>
2026-06-24 11:04:51 -0300

    Add files via upload

---
+++ java-main/davi/henrique.java
@@ -0,0 +1,8 @@
+public class henrique {
+     public static void main(String[] args) {
+        int energia = 0;
+        do{
+            System.out.println("testando");
+        }while(energia > 0);
+     }
+}
